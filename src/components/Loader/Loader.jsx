a6790d277b761e67f47d5282e60fd029ec149130
SzymanskaOlejniczak <aszymanskaolejniczak@gmail.com>
2023-01-07 17:50:06 +0100

    Update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -6,7 +6,7 @@ export function Loader() {
         <Grid
             height="80"
             width="80"
-            color="#4fa94d"
+            color="#5366ce"
             ariaLabel="grid-loading"
             radius="12.5"
             wrapperStyle={{}}
